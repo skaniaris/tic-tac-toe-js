f384c91568fcdbe9fab67cf961fa1fee1a9282e0
skaniaris <kaniariss@gmail.com>
2022-06-08 18:37:06 -0400

    starting js functionality start button, more styling and html

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 const gameBoard = (function() {
-    let gameArray = [X, O, X, O, X, O, X, O, X];
+    let gameArray = [["X", "O", "X"], ["O", "X", "O"], ["X", "O", "X"]];
+    
+    const render = () => {
+        let board = document.querySelector(".gameBoard");
+        for (let i = 0; i <gameArray.length; i++) {
+            board.innerHTML += gameArray[i] + "<br/>";
+        }
+    }
+    
+    let startBtn = document.querySelector(".start");
+    startBtn.addEventListener("click", render);
 })();
+
+const controller = (function() {
+    
+})();
+
+const player = (name, letter) => {
+    return {name, letter};
+};
